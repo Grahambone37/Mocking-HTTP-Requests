b20dc121f7b9fe117cca1b4ac94bd30459b393ce
Grahambone37 <electricgrunt37@gmail.com>
2023-02-25 01:12:46 -0800

    tested api-calls

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
 
 
 function App() {
+  const [gitHubName, setGitHubName] = useState('')
+  const [gitHubLink, setGitHubLink] = useState('')
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/Grahambone37')
+      .then(res => res.json())
+      .then(data => {
+        setGitHubName(data.name)
+        setGitHubLink(data.url)
+      })
+  }, [])
+
+
   return (
     <div className="App">
       <h1>Github Profile Info:</h1>
+      <h2>{gitHubName}</h2>
+      <a href={gitHubLink}>Link to GitHub account</a>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,29 @@
+/* eslint-disable testing-library/prefer-find-by */
+import App from './App'
+import { render, screen, waitFor } from "@testing-library/react";
 
+beforeEach(() => {
+    fetch.resetMocks()
+})
 
+describe('will show username under header', () => {
+    test('username fetched and put on page', async () => {
+        fetch.mockResponseOnce(JSON.stringify({ name: 'Graham Bone' }))
+        render(<App />)
+        let userName = await waitFor(() => screen.findByRole('heading', {level: 2}))
+
+        expect(userName.textContent).toBe('Graham Bone')
+    })
+})
+
+describe('button goes to github profile', () => {
+    test("button goes to github profile and says right thing", async () => {
+        fetch.mockResponseOnce(JSON.stringify({ url: "https://api.github.com/users/Grahambone37" }))
+        render(<App />)
+        let cite = await waitFor(() => screen.findByRole('link'))
+
+        expect(cite.textContent).toBe('Link to GitHub account')
+        expect(cite).toHaveAttribute('href', expect.stringContaining("https://api.github.com/users/Grahambone37"))
+    })
+})
 
